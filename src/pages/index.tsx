9a8f215263fc704a3c8e2f2c2a51d2837b570ed0
minsun309 <pminsun309@gmail.com>
2023-08-09 17:22:01 +0900

    tailwind test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,25 @@
-import Image from "next/image";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
-
 export default function Home() {
-  return <main>next.js</main>;
+  return (
+    <div className="bg-slate-400 py-20 px-10 grid gap-10">
+      <div className="bg-white p-10 rounded-2xl shadow-xl">
+        <span className="font-semibold text-3xl">Select Item</span>
+        <div className="flex justify-between my-2">
+          <span className="text-gray-500">Grey Chair</span>
+          <span>$19</span>
+        </div>
+        <div className="flex justify-between">
+          <span className="text-gray-500">Tooly Table</span>
+          <span>$80</span>
+        </div>
+        <div className="mt-2">
+          <span>Total</span>
+          <span>$99</span>
+        </div>
+        <div>Checkout</div>
+      </div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+    </div>
+  );
 }
