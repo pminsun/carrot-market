28942cdf0f96fad06e18dad0fb3ce4bb412ac36f
minsun309 <pminsun309@gmail.com>
2023-08-09 20:51:46 +0900

    tailwind test2

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,48 @@
 export default function Home() {
   return (
     <div className="bg-slate-400 py-20 px-10 grid gap-10">
-      <div className="bg-white p-10 rounded-2xl shadow-xl">
+      <div className="bg-white p-6 rounded-3xl shadow-xl">
         <span className="font-semibold text-3xl">Select Item</span>
         <div className="flex justify-between my-2">
           <span className="text-gray-500">Grey Chair</span>
-          <span>$19</span>
+          <span className="font-semibold">$19</span>
         </div>
         <div className="flex justify-between">
           <span className="text-gray-500">Tooly Table</span>
-          <span>$80</span>
+          <span className="font-semibold">$80</span>
         </div>
-        <div className="mt-2">
+        <div className="flex justify-between mt-2 pt-2 border-t-2 border-dashed">
           <span>Total</span>
-          <span>$99</span>
+          <span className="font-semibold">$99</span>
         </div>
-        <div>Checkout</div>
+        <div className="mt-5 bg-blue-500 text-white p-3 text-center rounded-xl w-1/2 mx-auto">
+          Checkout
+        </div>
       </div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
-      <div className="bg-white p-10 rounded-2xl shadow-xl"></div>
+      <div className="bg-white overflow-hidden rounded-2xl shadow-xl">
+        <div className="bg-blue-500 p-6 pb-14">
+          <span className="text-white text-2xl">Profile</span>
+        </div>
+        <div className="rounded-3xl p-6 bg-white relative -top-5">
+          <div className="flex relative -top-16 items-end justify-between">
+            <div className="flex flex-col items-center">
+              <span className="text-small text-gray-500">Orders</span>
+              <span className="font-medium">340</span>
+            </div>
+            <div className="h-24 w-24 bg-red-400 rounded-full" />
+            <div className="flex flex-col items-center">
+              <span className="text-small text-gray-500">Spent</span>
+              <span className="font-medium">$340</span>
+            </div>
+          </div>
+          <div className="relative flex flex-col items-center -mt-10 -mb-5">
+            <span className="text-lg font-medium">Tony Molloy</span>
+            <span className="text-small text-gray-500">미국</span>
+          </div>
+        </div>
+      </div>
+      <div className="bg-white p-6 rounded-2xl shadow-xl"></div>
+      <div className="bg-white p-6 rounded-2xl shadow-xl"></div>
     </div>
   );
 }
