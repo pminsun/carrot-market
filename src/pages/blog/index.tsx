468a5052ca4ca754d1cc00b98f780457027be9f5
minsun309 <pminsun309@gmail.com>
2023-09-25 13:52:14 +0900

    NEXTJS DEEP DIVE 11-15

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -3,11 +3,13 @@ import { readFileSync, readdirSync } from "fs";
 import path from "path";
 import matter from "gray-matter";
 import { NextPage } from "next";
+import Link from "next/link";
 
 interface Post {
   title: string;
   date: string;
   category: string;
+  slug: string;
 }
 
 const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
@@ -17,12 +19,14 @@ const Blog: NextPage<{ posts: Post[] }> = ({ posts }) => {
       <ul>
         {posts.map((post, index) => (
           <div key={index} className="mb-5">
-            <span className="text-lg text-red-500">{post.title}</span>
-            <div>
-              <span>
-                {post.date} / {post.category}
-              </span>
-            </div>
+            <Link href={`/blog/${post.slug}`}>
+              <span className="text-lg text-red-500">{post.title}</span>
+              <div>
+                <span>
+                  {post.date} / {post.category}
+                </span>
+              </div>
+            </Link>
           </div>
         ))}
       </ul>
@@ -34,7 +38,8 @@ export async function getStaticProps() {
   const postsDirectory = path.join(process.cwd(), "src/posts");
   const blogPosts = readdirSync(postsDirectory).map((file) => {
     const content = readFileSync(`src/posts/${file}`, "utf-8");
-    return matter(content).data;
+    const [slug, _] = file.split(".");
+    return { ...matter(content).data, slug };
   });
   return {
     props: {
